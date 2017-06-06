83d94134c485a3162f5e1e3a4e9e1dfc88fd0f14
唐云 <tangyun@speedto.cn>
2017-06-06 10:54:23 +0800

    对about进行了排版

--- client/components/about/index.js
+++ client/components/about/index.js
@@ -8,7 +8,7 @@ export class HomeView extends Component {
 	render() {
 		return (
 		  <div className={CSS["recruit"]}>
-		    <PageTitle title='JOIN US TO EXPERIMENT & MAKE.' />
+		    <PageTitle title='ABOUT' />
 		    <QueueAnim
 	  			key={"bannerAnimate"}
 	  			delay={200}
@@ -28,23 +28,23 @@ export class HomeView extends Component {
 	  			duration={1500}
 	  			type='top'
 	  		>
-			    <div className={CSS["my-contains"]}  key={"my-contains"}>
+			    <div className={CSS["my-contains"]} key={"my-contains"}>
             <div className={CSS["about-page"]}>
               <div className={CSS["h4"]}>INTRODUCTION</div>
-              <div className={CSS["about-desc"]} dangerouslySetInnerHTML={{__html: '北京博尚广告有限公司成立于2011年，是一家以创意为导向，立足于Social Media，专注于品牌策划、管理和推广，致力于提供互动行销整体解决方案的数字营销公司。公司核心团队中60%的创意人员来自4A，8年以上广告从业经历的资深成员达40%，且拥有专业而独立的互动技术团队，在Digital和CRM领域均有自己独到的运营理念。<br />博尚广告成立以来，紧扣社交媒体发展的脉搏，集合丰富且全面的媒体推广资源，整合创新，一直走在数字营销前沿，并于2013年被艾菲奖组委会推选，荣登大中华区4A 100强。'}}></div>
+              <div className={CSS["about-desc"]} dangerouslySetInnerHTML={{__html: '<p>北京博尚广告有限公司成立于2011年，是一家以创意为导向，立足于Social Media，专注于品牌策划、管理和推广，致力于提供互动行销整体解决方案的数字营销公司。公司核心团队中60%的创意人员来自4A，8年以上广告从业经历的资深成员达40%，且拥有专业而独立的互动技术团队，在Digital和CRM领域均有自己独到的运营理念。</p><p>博尚广告成立以来，紧扣社交媒体发展的脉搏，集合丰富且全面的媒体推广资源，整合创新，一直走在数字营销前沿，并于2013年被艾菲奖组委会推选，荣登大中华区4A 100强。</p>'}}></div>
               <div className={CSS["h4"]}>SERVICES</div>
-              <div className="services">
+              <div className={CSS["services"]}>
                 <Grid fluid>
-                  <Row>
-                    <Col md={3}>品牌策略</Col>
-                    <Col md={3}>APP制作推广</Col>
-                    <Col md={3}>微博/微信运营</Col>
-                    <Col md={3}>媒体投放</Col>
-                    <Col md={3}>TVC制作</Col>
-                    <Col md={3}>EPR营销</Col>
-                    <Col md={3}>SEM营销</Col>
-                    <Col md={3}>活动执行</Col>
-                    <Col md={3}>网站制作</Col>
+                  <Row className={CSS["services-row"]}>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>品牌策略</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>APP制作推广</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>微博/微信运营</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>媒体投放</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>TVC制作</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>EPR营销</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>SEM营销</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>活动执行</div></Col>
+                    <Col xs={6} sm={4} md={3}><div className={CSS["content"]}>网站制作</div></Col>
                   </Row>
                 </Grid>
               </div>
