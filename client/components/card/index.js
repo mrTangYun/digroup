a4e0b8074b35ccb4a42692ecef40fc1011fce7c2
唐云 <tangyun@speedto.cn>
2017-06-20 15:38:38 +0800

    样式的修改，以及案例图片

--- client/components/card/index.js
+++ client/components/card/index.js
@@ -47,7 +47,7 @@ export class Card extends Component {
     const props = this.props.data;
     const { show, imgLoaded } = this.state;
     return (<div
-      className={CSS.column + ' col-lg-3 col-md-4  col-sm-6 col-xs-12 ' + (show ? 'flipInX3' : 'flipContainer')}
+      className={CSS.column + ' col-lg-4 col-md-4  col-sm-6 col-xs-12 ' + (show ? 'flipInX3' : 'flipContainer')}
       style={{
         animationDelay: (this.props.delay || 0) + 's'
       }}
--- client/components/works/index.js
+++ client/components/works/index.js
@@ -6,70 +6,69 @@ import Data from '../../static/works.json';
 
 export class HomeView extends Component {
 
-	constructor(props) {
-		super(props);
-		this.delayArray = [];
-		this.canRun = true;
-		this.tmpArray = [];
-		this.state = {
-			delayObject: Data.map((item, index) => {
-				return (index + 1) / 6
-			})
-		};
-	}
+  constructor (props) {
+    super(props);
+    this.delayArray = [];
+    this.canRun = true;
+    this.tmpArray = [];
+    this.state = {
+      delayObject: Data.map((item, index) => {
+        return (index + 1) / 6;
+      })
+    };
+  }
 
-	isIntersection (index) {
-		this.delayArray = [...this.delayArray, index];
-	    this.tmpArray = [...this.tmpArray, index];
-		if(!this.canRun){
-	        // 判断是否已空闲，如果在执行中，则直接return
-	        return;
-	    }
-	    this.canRun = false;
-	    setTimeout(() => {
-	        // console.log("函数节流");
-	        const tmpArray_ = this.tmpArray.sort((a, b) => a - b);
-	        const delayObject = this.state.delayObject.slice(0);
-	        // console.log(tmpArray_);
-	        tmpArray_.map((item, index) => {
-	        	delayObject[item] = (index + 1) / 6
-	        });
+  isIntersection (index) {
+    this.delayArray = [...this.delayArray, index];
+    this.tmpArray = [...this.tmpArray, index];
+    if (!this.canRun) {
+      // 判断是否已空闲，如果在执行中，则直接return
+      return;
+    }
+    this.canRun = false;
+    setTimeout(() => {
+      // console.log("函数节流");
+      const tmpArray_ = this.tmpArray.sort((a, b) => a - b);
+      const delayObject = this.state.delayObject.slice(0);
+      // console.log(tmpArray_);
+      tmpArray_.map((item, index) => {
+        delayObject[item] = (index + 1) / 6;
+      });
 
-	        // console.log(delayObject);
-	        this.setState({
-	         	delayObject: delayObject
-	        });
-	        this.canRun = true;
-	    	this.tmpArray = [];
-	    }, 1000 / 2);
-	}
-	render () {
-		const { delayObject } = this.state;
-		// console.log(delayObject);
-	    return (
-	      	<div className={CSS['page-works']}>
-			  	<PageTitle title='SELECTED WORKS' />
-			    <div className='container-fluid'>
-				    <div className='row-fluid'>
-				    {
-				    	Data.map((item, index) => {
-				    		return (<Card
-					    		key={index}
-						    	style={{
-						    		width: '50%'
-						    	}}
-						    	isIntersection={ () =>{this.isIntersection(index)}}
-						    	delay={delayObject[index]}
-						    	show={false}
-	                			data={item}
-						    />);
-				    	})
-				    }
-				    </div>
-			    </div>
-			</div>
-		);
-	}
+      // console.log(delayObject);
+      this.setState({
+        delayObject: delayObject
+      });
+      this.canRun = true;
+      this.tmpArray = [];
+    }, 1000 / 2);
+  }
+  render () {
+    const { delayObject } = this.state;
+    return (
+      <div className={CSS['page-works']}>
+        <PageTitle title='SELECTED WORKS' />
+        <div className='container-fluid'>
+          <div className='row-fluid'>
+            {
+              Data.map((item, index) => {
+                return (<Card
+                  key={index}
+                  style={{
+                    width: '50%'
+                  }}
+                  isIntersection={() => { this.isIntersection(index); }}
+                  delay={delayObject[index]}
+                  show={false}
+                  data={item}
+                />);
+              })
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default HomeView;
