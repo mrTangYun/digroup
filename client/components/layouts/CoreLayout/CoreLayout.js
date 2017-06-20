9af2a6ef728efdf9037767350ac0987f94f66bf7
唐云 <tangyun@speedto.cn>
2017-06-20 17:08:45 +0800

    接上

--- client/components/layouts/CoreLayout/CoreLayout.js
+++ client/components/layouts/CoreLayout/CoreLayout.js
@@ -4,37 +4,29 @@ import QueueAnim from 'rc-queue-anim';
 import Home from '../../works/';
 
 export const CoreLayout = ({ location, children }) => {
-	const key = location.pathname;
-  	const keys = key.replace('/', '') ? [key.replace('/', '')] : ['home'];
-	return(
-	  <div className='container-fluid'>
-	    <Header />
-	    <QueueAnim
-	    	duration={1500}
-	    	delay={500}
-	    	type='top'
-	    >
-		    <div key='childrenOuter'>
-				{children || <Home />}
-		    </div>
-	    </QueueAnim>
-	    {/*<QueueAnim
-	    	duration={500}
-	    	ease={['easeOutQuart', 'easeInOutQuart']}
-	    	delay={[500, 0]}
-	    	// animConfig={[
-	     //        { opacity: [1, 0], translateY: [0, 50] },
-	     //        { opacity: [1, 0], translateY: [0, -50] }
-	     //      ]}
-	    	>
-	    	{React.cloneElement(children || <Home />, { key })}
-		</QueueAnim>*/}
-	  </div>
-	)
+  const key = location.pathname;
+  const keys = key.replace('/', '') ? [key.replace('/', '')] : ['home'];
+  const isWorkListpage = keys[0] === 'home' || keys[0] === 'works';
+  const containerStyle = isWorkListpage ? 'container-fluid' : 'container';
+  return (
+    <div className={containerStyle}>
+      <Header isWorkListpage={isWorkListpage} />
+      <QueueAnim
+        duration={1500}
+        delay={500}
+        type='top'
+      >
+        <div key='childrenOuter'>
+          {children || <Home />}
+        </div>
+      </QueueAnim>
+    </div>
+  );
 };
 
 CoreLayout.propTypes = {
-  children: React.PropTypes.element
+  children: React.PropTypes.element,
+  location: React.PropTypes.object
 };
 
 export default CoreLayout;
